098fc1cc294b25d6b97c409479b6a7e54a59645e
Alexandre Janin <alexandrejanin92@gmail.com>
2018-07-14 22:52:06 +0200

    Formatting

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,69 @@
 extern crate sdl2;
 
-use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
 
 use std::time::Duration;
 
 mod input;
 
-
 pub fn main() {
-	//Create context and init video
-	let sdl_context = sdl2::init().unwrap();
-	let video_subsystem = sdl_context.video().unwrap();
+    //Create context and init video
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
     let max_fps = 60;
 
-	//Create window
-	let window = video_subsystem.window("Rust/SDL2 Game", 800, 600)
-		.position_centered()
-		.opengl()
-		.build()
-		.unwrap();
+    //Create window
+    let window = video_subsystem
+        .window("Rust/SDL2 Game", 800, 600)
+        .position_centered()
+        //.opengl()
+        .build()
+        .unwrap();
 
-	//Create canvas for window
-	let mut canvas = window.into_canvas().build().unwrap();
-	canvas.set_draw_color(Color::RGB(255, 0, 0));
-	canvas.clear();
-	canvas.present();
-	
-	//Create event pump for window
-	let mut event_pump = sdl_context.event_pump().unwrap();
+    //Create canvas for window
+    let mut canvas = window.into_canvas().build().unwrap();
+    canvas.set_draw_color(Color::RGB(255, 0, 0));
+    canvas.clear();
+    canvas.present();
+
+    //Create event pump for window
+    let mut event_pump = sdl_context.event_pump().unwrap();
 
     //Create input manager
     let mut input_manager = input::InputManager::new();
 
-	'main_loop: loop {
-		for event in event_pump.poll_iter() {
-			match event {
-				Event::Quit {..} | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-					break 'main_loop
-				},
+    'main_loop: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'main_loop,
 
-				_ => {}
-			}
-		}
-		// The rest of the game loop goes here...
-	    input_manager.update(&event_pump);
+                _ => {}
+            }
+        }
+        
+        // The rest of the game loop goes here...
+        input_manager.update(&event_pump);
 
-        if input_manager.get_key_down(Keycode::Up) {canvas.set_draw_color(Color::RGB(0, 255, 0))}
-        else if input_manager.get_key_down(Keycode::Down) {canvas.set_draw_color(Color::RGB(0, 0, 255))}
-        else {canvas.set_draw_color(Color::RGB(0, 0, 0))}
+        if input_manager.get_key_pressed(Keycode::Space) {
+            canvas.set_draw_color(Color::RGB(255, 0, 0))
+        } else if input_manager.get_key_down(Keycode::Space) {
+            canvas.set_draw_color(Color::RGB(0, 0, 255))
+        } else {
+            canvas.set_draw_color(Color::RGB(0, 0, 0))
+        }
 
-		canvas.clear();
+        canvas.clear();
 
-		//Draw stuff
+        //Draw stuff
 
-		canvas.present(); 
+        canvas.present();
 
-		std::thread::sleep(Duration::from_micros(1_000_000 / max_fps));
-	}
+        std::thread::sleep(Duration::from_micros(1_000_000 / max_fps));
+    }
 }
